4c8f9552a5642f3f73f0ffa082773d9fa2fbbaf2
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2022-09-23 01:24:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="iphone_body">
+        <div className="iphone_cam">
+          <div className="cam1_cam2">
+            <div className="cam1"></div>
+            <div className="cam2"></div>
+          </div>
+          <div className="cam3">
+            <div className="flash"></div>
+            <div className="telephoto"></div>
+          </div>
+        </div>
+
+        <div className="apple_logo">
+          <div className="leafs">
+            <div className="leaf"></div>
+            <div className=" leaf_2"></div>
+          </div>
+          <div className="apple">
+            <div className="left_slice"></div>
+            <div className="right_slice"></div>
+            <div className="cut"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
